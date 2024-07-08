5e2d2f1836711b05f0a4aba83abaa089f96cb0ee
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-07-08 23:31:39 +0100

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config(); // Load environment variables from .env file
+require("dotenv").config(); 
 const pg = require("pg");
-// let sequelize;
-
-// For production deployment on Render.com or similar
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
