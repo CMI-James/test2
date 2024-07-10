6346d5c1bde53fcb029317a06ea2da72ffff9bea
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-07-10 03:42:08 +0100

    okay

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { POSTGRES_DATABASE, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+const sequelize = new Sequelize(POSTGRES_DATABASE, POSTGRES_USER, POSTGRES_PASSWORD, {
+  host: POSTGRES_HOST,
   dialect: "postgres",
   dialectModule: require("pg"),
   define: {
@@ -15,3 +15,22 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 });
 
 module.exports = { sequelize };
+
+
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//   host: DB_HOST,
+//   dialect: "postgres",
+//   dialectModule: require("pg"),
+//   define: {
+//     freezeTableName: true,
+//     timestamps: true,
+//   },
+//   logging: false,
+// });
+
+// module.exports = { sequelize };
